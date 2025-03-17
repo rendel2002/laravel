4f9c38a2dad5a3110aa22badc301a5ad494f7891
rendel2002 <140384438+rendel2002@users.noreply.github.com>
2025-03-17 14:17:48 +0800

    Update PDFController.php

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -7,10 +7,17 @@ use Barryvdh\DomPDF\Facade\Pdf;
 
 class PDFController extends Controller
 {
-    public function generatePDF()
+    // Function for Inventory Slip PDF
+    public function generateInventoryPDF()
     {
         $pdf = Pdf::loadView('pdf.inventory_slip');
         return $pdf->stream('inventory_slip.pdf');
     }
-}
 
+    // Function for Journal Entry PDF
+    public function generateJournalEntryPDF()
+    {
+        $pdf = Pdf::loadView('pdf.journal_entry');
+        return $pdf->stream('journal_entry.pdf');
+    }
+}
