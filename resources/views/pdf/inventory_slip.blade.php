d2c03ae34cf2443ab413f8f871e89eb9b2af3526
rendel2002 <140384438+rendel2002@users.noreply.github.com>
2025-03-20 09:03:44 +0800

    Update inventory_slip.blade.php

--- resources/views/pdf/inventory_slip.blade.php
+++ resources/views/pdf/inventory_slip.blade.php
@@ -3,132 +3,90 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Inventory Custodian Slip</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        .appendix {
-            font-style: italic;
-            font-size: 16px;
-            position: absolute;
-            right: 30px;
-        }
-
-        .box {
-            width: 100%;
-            margin-top: 15px;
-            max-width: 670px;
-            padding: 10px;
-            text-align: center;
-            font-size: 1rem;
-
-        }
-
-        .header-box {
-            border: 2px solid black;
-            padding: 10px;
-            text-align: center;
-        }
-
-        .header-info1 {
-            display: flex;
-            margin-top: 10px;
-            justify-content: space-between;
-        }
-
-        .header-info1 div {
-            flex: 1;
-            text-align: left;
-        }
-
-        .table-container {
-            width: 100%;
-        }
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        th,
-        td {
-            border: 2px solid black;
-            padding: 5px;
-            text-align: center;
-        }
-
-        th {
-            background-color: rgb(255, 255, 255);
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+</style>
 
-<body class="d-flex justify-content-center align-items-center vh-100">
+<body>
 
-    <span class="appendix">Appendix 52</span>
-    <div class="box">
-        <div class="header-box">
-            <h5 style="font-size: 1rem;">INVENTORY CUSTODIAN SLIP</h5>
-            <div class="header-info1">
-                <div>LGU: ____________________</div>
-                <div>ICS No: ____________________</div>
-                <div style="text-align: right">Fund: ____________________</div>
-            </div>
-        </div>
 
-        <div class="table-container">
-            <table>
-                <tr>
-                    <th rowspan="2">QTY</th>
-                    <th rowspan="2">Unit</th>
-                    <th colspan="2">Amount</th>
-                    <th rowspan="2">Description</th>
-                    <th rowspan="2">Inventory Item No.</th>
-                    <th rowspan="2">Estimated Useful Life</th>
-                </tr>
-                <tr>
-                    <th>Unit Cost</th>
-                    <th>Total Cost</th>
-                </tr>
-                <tr>
-                    <td style="height: 400px;">1</td>
-                    <td style="height: 400px;">PC</td>
-                    <td style="height: 400px;">40000</td>
-                    <td style="height: 400px;">40000</td>
-                    <td style="height: 400px;">Computer Monitor</td>
-                    <td style="height: 400px;">5</td>
-                    <td style="height: 400px;">5</td>
-                </tr>
-
-            </table>
-        </div>
-
-        <table style="width: 100%; text-align: center; border-collapse: collapse;">
-            <tr>
-                <td style="border: 2px solid black;">
-                    <div style="text-align: left; font-weight: bold;">Received from:</div>
-                    <div style="border-bottom: 2px solid black; width: 70%; margin: 30px auto;"></div>
-                    <div>Signature Over Printed Name</div>
-                    <div style="border-bottom: 2px solid black; width: 70%; margin: 30px auto;"></div>
-                    <div>Position/Office</div>
-                    <div style="border-bottom: 2px solid black; width: 70%; margin: 30px auto;"></div>
-                    <div>Date</div>
-                </td>
-                <td style="border: 2px solid black;">
-                    <div style="text-align: left; font-weight: bold;">Received by:</div>
-                    <div style="border-bottom: 2px solid black; width: 70%; margin: 30px auto;"></div>
-                    <div>Signature Over Printed Name</div>
-                    <div style="border-bottom: 2px solid black; width: 70%; margin: 30px auto;"></div>
-                    <div>Position/Office</div>
-                    <div style="border-bottom: 2px solid black; width: 70%; margin: 30px auto;"></div>
-                    <div>Date</div>
-                </td>
-            </tr>
-
-        </table>
-        <div style="text-align: left; font-weight: bold;">For Use of Supply and/or Property Division/Unit </div>
-
+    <div style="text-align: right;">
+        <span style="font-style: italic; font-size: 16px;">Appendix 52</span>
     </div>
+    <table style="width:100%; table-layout: fixed;">
+        <tr>
+            <td colspan="14" style="text-align: center; font-weight: bold; font-size: 1.2rem; padding-top: 10px; font-family: Calibri, sans-serif;">
+                INVENTORY CUSTODIAN SLIP
+                <div style="display: flex; justify-content: space-between; font-size: small; font-weight: normal; margin-top: 10px;">
+                    <!-- Left Side (LGU & Fund) -->
+                    <div style="text-align: left; padding-top: 20px">
+                        <p style="margin: 0;">LGU: __________________</p>
+                        <p style="margin: 0;">Fund: __________________</p>
+                    </div>
+                    <!-- Right Side (ICS No aligned with Fund) -->
+                    <div style="text-align: right; display: flex; align-items: center; padding-right: 20px; padding-bottom: 5px">
+                        <p style="margin: 0;">ICS No: __________________</p>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <th rowspan="2" colspan="1">QTY</th>
+            <th rowspan="2" colspan="1">Unit</th>
+            <th colspan="4">Amount</th>
+            <th rowspan="2" colspan="4">Description</th>
+            <th rowspan="2" colspan="2">Inventory Item No.</th>
+            <th rowspan="2" colspan="2">Estimated Useful Life</th>
+        </tr>
+        <tr>
+            <th colspan="2">Unit Cost</th>
+            <th colspan="2"> Total Cost</th>
+        </tr>
+
+        <tr>
+            <td style=" height: 50%;"></td>
+            <td></td>
+            <td colspan="2"></td>
+            <td colspan="2"></td>
+            <td colspan="4"></td>
+            <td colspan="2"></td>
+            <td colspan="2"></td>
+        </tr>
+
+
+        <tr>
+            <td colspan="7" style="text-align: center; vertical-align: top;">
+                <div style="text-align: left; font-weight: bold; margin-bottom: 10px;">Received from:</div>
+                <div style="margin: 5px auto;">____________________________</div>
+                <div>Signature Over Printed Name</div>
+                <div style="margin: 5px auto;">____________________________</div>
+                <div>Position/Office</div>
+                <div style="margin: 5px auto;">____________________________</div>
+                <div>Date</div>
+            </td>
+
+            <td colspan="7" style="text-align: center; vertical-align: top;">
+                <div style="text-align: left; font-weight: bold; margin-bottom: 10px;">Received by:</div>
+                <div style="margin: 5px auto;">____________________________</div>
+                <div>Signature Over Printed Name</div>
+                <div style="margin: 5px auto;">____________________________</div>
+                <div>Position/Office</div>
+                <div style="margin: 5px auto;">____________________________</div>
+                <div>Date</div>
+            </td>
+
+        </tr>
+
+    </table>
+    <div style="text-align: left; font-weight: bold;">For Use of Supply and/or Property Division/Unit </div>
 
 </body>
 
