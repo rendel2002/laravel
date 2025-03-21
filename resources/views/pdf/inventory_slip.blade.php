b5e9b9df1c9f3fcce533ca095c00c06725cf5d26
rendel2002 <140384438+rendel2002@users.noreply.github.com>
2025-03-21 16:42:01 +0800

    Update inventory_slip.blade.php

--- resources/views/pdf/inventory_slip.blade.php
+++ resources/views/pdf/inventory_slip.blade.php
@@ -19,52 +19,58 @@
 
 
     <div style="text-align: right;">
-        <span style="font-style: italic; font-size: 16px;">Appendix 52</span>
+        <span style="font-style: italic; font-size: 13px;">Appendix 52</span>
     </div>
-    <table style="width:100%; table-layout: fixed;">
+    <table style="width:100%; table-layout: fixed; padding-top: 15px">
         <tr>
-            <td colspan="14" style="text-align: center; font-weight: bold; font-size: 1.2rem; padding-top: 10px; font-family: Calibri, sans-serif;">
+            <!--  (HEADER) -->
+            <td colspan="16" style="text-align: center; font-weight: bold; font-size: 1.2rem; padding-top: 10px; ">
                 INVENTORY CUSTODIAN SLIP
-                <div style="display: flex; justify-content: space-between; font-size: small; font-weight: normal; margin-top: 10px;">
-                    <!-- Left Side (LGU & Fund) -->
-                    <div style="text-align: left; padding-top: 20px">
-                        <p style="margin: 0;">LGU: __________________</p>
-                        <p style="margin: 0;">Fund: __________________</p>
+                <div style="display: flex; justify-content: space-between; font-size: small; font-weight: normal; margin-top: 10px; align-items: flex-start;">
+                    <!-- (LGU, Fund and ICS No in line) -->
+                    <div style="text-align: left; padding-top: 20px; width: 100%; font-size: 13px">
+                        <p style="margin: 0;"><strong>LGU:</strong> ____________________________________</p>
+                        <div style="display: flex; justify-content: space-between; align-items: center; margin-top: 5px; padding-bottom: 3px;">
+                            <span style="margin: 0;"><strong>Fund:</strong> ____________________________________</span>
+                            <span style="margin: 0; text-align: right; padding-left: 30%;"><strong style="padding-left: 30%; ">ICS No:</strong> ______________________</span>
+                        </div>
                     </div>
-                    <!-- Right Side (ICS No aligned with Fund) -->
-                    <div style="text-align: right; display: flex; align-items: center; padding-right: 20px; padding-bottom: 5px">
-                        <p style="margin: 0;">ICS No: __________________</p>
-                    </div>
+
                 </div>
+
             </td>
         </tr>
-        <tr>
-            <th rowspan="2" colspan="1">QTY</th>
-            <th rowspan="2" colspan="1">Unit</th>
+
+        <!-- Column headers for entries -->
+        <tr style="font-size: 13px;">
+            <th rowspan="2" colspan="2">Quantity</th>
+            <th rowspan="2" colspan="2">Unit</th>
             <th colspan="4">Amount</th>
             <th rowspan="2" colspan="4">Description</th>
             <th rowspan="2" colspan="2">Inventory Item No.</th>
             <th rowspan="2" colspan="2">Estimated Useful Life</th>
         </tr>
-        <tr>
+        <tr style="font-size: 13px;">
             <th colspan="2">Unit Cost</th>
             <th colspan="2"> Total Cost</th>
         </tr>
 
+
+        <!-- ROWS  -->
         <tr>
-            <td style=" height: 50%;"></td>
-            <td></td>
-            <td colspan="2"></td>
-            <td colspan="2"></td>
-            <td colspan="4"></td>
-            <td colspan="2"></td>
-            <td colspan="2"></td>
+            <td colspan="2" style=" height: 50%;"></td> <!-- table for  Quantity -->
+            <td colspan="2"></td> <!-- table for Unit  -->
+            <td colspan="2"></td> <!-- table for Unit Cost  -->
+            <td colspan="2"></td> <!-- table for Total Cost  -->
+            <td colspan="4"></td> <!-- table for Description  -->
+            <td colspan="2"></td> <!-- table for Inventory Item No. -->
+            <td colspan="2"></td> <!-- table for Estimated Useful Life  -->
         </tr>
 
-
+        <!-- FOOTER  -->
         <tr>
-            <td colspan="7" style="text-align: center; vertical-align: top;">
-                <div style="text-align: left; font-weight: bold; margin-bottom: 10px;">Received from:</div>
+            <td colspan="8" style="text-align:  center; font-size: 15px; vertical-align: top;">
+                <div style="text-align: left; margin-bottom: 10px;">Received from:</div>
                 <div style="margin: 5px auto;">____________________________</div>
                 <div>Signature Over Printed Name</div>
                 <div style="margin: 5px auto;">____________________________</div>
@@ -73,8 +79,8 @@
                 <div>Date</div>
             </td>
 
-            <td colspan="7" style="text-align: center; vertical-align: top;">
-                <div style="text-align: left; font-weight: bold; margin-bottom: 10px;">Received by:</div>
+            <td colspan="8" style="text-align:  center; font-size: 15px; vertical-align: top;">
+                <div style="text-align: left;  margin-bottom: 10px;">Received by:</div>
                 <div style="margin: 5px auto;">____________________________</div>
                 <div>Signature Over Printed Name</div>
                 <div style="margin: 5px auto;">____________________________</div>
@@ -86,7 +92,7 @@
         </tr>
 
     </table>
-    <div style="text-align: left; font-weight: bold;">For Use of Supply and/or Property Division/Unit </div>
+    <div style="text-align: left; font-size: 13px; font-style: italic;">For Use of Supply and/or Property Division/Unit </div>
 
 </body>
 
